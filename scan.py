a6670b86dbc879e626fd5f9ed60e852e925e2cab
Aduno <sanp100d@gmail.com>
2021-05-13 10:32:37 -0400

    Added memory express and AMD cards

--- scan.py
+++ scan.py
@@ -29,7 +29,6 @@ def bestBuy(search):
     ua = UserAgent()
     opts = Options()
     opts.add_argument("user-agent="+ua.random)
-    opts.add_argument("headless")
     driver = webdriver.Chrome(options=opts)
     driver.get(search)
     time.sleep(2)
@@ -57,20 +56,31 @@ def bestBuy(search):
 
         time.sleep(random.randint(17,28))
         driver.refresh()
+
+def memExp(search):
+    link=""
+    ua = UserAgent()
+    opts = Options()
+    opts.add_argument("user-agent="+ua.random)
+    driver = webdriver.Chrome(options=opts)
+    driver.get(search)
+    time.sleep(2)
+    loop = True
         
 def timer():
     while True:
         print(datetime.now().strftime('%d %H:%M:%S'))
         time.sleep(60)
+
 def readFile():
-    f = open("info.txt","r")
     user = f.readline()
     password = f.readline()
     phone = f.readline()
+    return (user,password,phone)
 
+def inputCheck():
+    loop=False
 if __name__==  "__main__":
-    i=0
-    loop=True
     gpuBestBuy ={
         "3060": "https://www.bestbuy.ca/en-ca/category/graphics-cards/20397?path=category%253AComputers%2B%2526%2BTablets%253Bcategory%253APC%2BComponents%253Bcategory%253AGraphics%2BCards%253Bcustom0graphicscardmodel%253AGeForce%2BRTX%2B3060",
         "3060ti": "https://www.bestbuy.ca/en-ca/category/graphics-cards/20397?path=category%253AComputers%2B%2526%2BTablets%253Bcategory%253APC%2BComponents%253Bcategory%253AGraphics%2BCards%253Bcustom0graphicscardmodel%253AGeForce%2BRTX%2B3060%2BTi",
@@ -81,39 +91,65 @@ if __name__==  "__main__":
     gpuMemoryExp ={
         "3060": "https://www.memoryexpress.com/Category/VideoCards?FilterID=f1b0a6e4-f41e-5fea-c242-d1bac7b02bf2",
         "3060ti": "https://www.memoryexpress.com/Category/VideoCards?FilterID=75668704-944f-8e1a-4cca-036beb9638a8",
+        "3070": "https://www.memoryexpress.com/Category/VideoCards?FilterID=e3034e65-f2ac-35f1-26eb-277b7a7e9ce9",
+        "3080": "https://www.memoryexpress.com/Category/VideoCards?FilterID=45788ec3-6bb1-e460-abe6-afa274b9d30e",
+        "3090": "https://www.memoryexpress.com/Category/VideoCards?FilterID=0faf222f-0400-d211-b926-04fdfc0bfa85",
         "6700xt": "https://www.memoryexpress.com/Category/VideoCards?FilterID=0901d9d6-31e0-987f-382c-e66e7ee23a8a",
         "6800": "https://www.memoryexpress.com/Category/VideoCards?FilterID=8d5ba2df-0447-8b14-4791-aee8db2800b0",
         "6800xt": "https://www.memoryexpress.com/Category/VideoCards?FilterID=9705ada8-e2b2-0ac9-738e-5e92c99a5932",
         "6900xt": "https://www.memoryexpress.com/Category/VideoCards?FilterID=a546466e-2a58-905e-129b-fc735319acbf"
         
     }
+    validInputs = ["3060","3060ti","3070","3080","3090","6700xt","6800","6800xt","6900xt","done"]
     searchList =[]
     currentTime = threading.Thread(target=timer)
-    readFile()
+    
     f = open("info.txt","r")
-    user = f.readline()
-    password = f.readline()
-    phone =  f.readline()
+    user,password,phone = readFile()
+    f.close()
 
     print("What GPU are you looking for?")
     print("Please enter one at a time. Once done, enter \"done\"")
-    print("Available options: 3060,3060ti,3070,3080,3090: ")
+    print("Available options:")
+    print("Nvidia: 3060,3060ti,3070,3080,3090")
+    print("AMD: 6700xt,6800,6800xt,6900xt\n")
+
+    loop=True
     while loop:
         search = input()
-        while(search!="3060" and search!="3060ti" and search!="3070" and search!="3080" and search!="3090" and search!="done"):
+        search = search.lower()
+        while(search not in validInputs):
             search = input("Invalid input. Please select from the list provided: ")
         if (search=="done"):
             loop=False
         else:
             searchList.append(search)
-
+    
+    bestList=[]
+    memList=[]
     for product in searchList:
-        searchList[i] = gpuLinks[product] 
-        i+=1
+        try:
+            bestList.append(gpuBestBuy[product]) 
+        except:
+            pass
+        try:
+            memList.append(gpuMemoryExp[product])
+        except:
+            pass
+    del searchList
 
-    for i,name in enumerate(searchList):
-        best = threading.Thread(target=bestBuy, args=[searchList[i]])
-        best.start()
-        time.sleep(2)
+
+    if(len(bestList)>0):
+        for i,name in enumerate(bestList):
+            best = threading.Thread(target=bestBuy, args=[bestList[i]])
+            best.start()
+            time.sleep(1)
+
+    if(len(memList)>0):
+        for i,name in enumerate(memList):
+            mem = threading.Thread(target=memExp, args=[memList[i]])
+            mem.start()
+            time.sleep(1)
+
     print("Beginning search...")
     currentTime.start()
